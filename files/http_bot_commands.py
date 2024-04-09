6ce4721350c348dbe74c23e81d8f9bfe4a37ff5f
Chelcea Calin Marius <chelcea.calin@yahoo.com>
2024-04-09 18:23:10 +0300

    Update http_bot_commands.py

--- files/http_bot_commands.py
+++ files/http_bot_commands.py
@@ -11,7 +11,7 @@ Quote = Dict[str, str]
 Zen_Quotes_Url="https://zenquotes.io/api/random"
 
 def get_quote():
-    response=requests.get('Zen_Quotes_Url')
+    response=requests.get(Zen_Quotes_Url)
     json_data = json.loads(response.text)
     quote: Quote = json_data[0]['q'] + " - " + json_data[0]['a']
     return quote
